0506b10e36424f04ba11ab022d74d19ebc5aa6fe
edilaitin <laitineduard@gmail.com>
2022-10-30 21:43:36 +0200

    Initial commit

---
+++ graphNormalizer/graph.py
@@ -0,0 +1,81 @@
+import json
+import os
+from copy import deepcopy
+from random import choices
+
+
+class Node:
+    def __init__(self, node_id, label):
+        self.id = node_id
+        self.label = label
+
+    def __str__(self):
+        return f'id: {self.id}, label: {self.label}'
+
+    def get_label(self):
+        return self.label
+
+
+class Edge:
+    def __init__(self, node1, node2):
+        self.node1 = node1
+        self.node2 = node2
+
+    def __str__(self):
+        return f'Edge: {self.node1.id} - {self.node2.id}'
+
+    def as_array(self):
+        return [self.node1.id, self.node2.id]
+
+
+class Graph:
+    def __init__(self, filename, component_nodes, vm_nodes, adjacency_matrix):
+        self.filename = filename
+        self.nodes = component_nodes + vm_nodes
+        self.edges = []
+        index_offset = len(component_nodes) + 1
+        self.vm_index_start = index_offset
+        for i, _ in enumerate(adjacency_matrix):
+            for j, _ in enumerate(adjacency_matrix[i]):
+                if adjacency_matrix[i][j] == 1:
+                    node1 = next((x for x in component_nodes if x.id == i + 1))
+                    node2 = next((x for x in vm_nodes if x.id == j + index_offset))
+                    self.edges.append(Edge(node1, node2))
+
+    def edit_graph(self):
+        ged = 0
+        nr_nodes_added_pop = [0, 1, 2]
+        weight_nodes = [0.5, 0.35, 0.15]
+        add_nodes = choices(nr_nodes_added_pop, weight_nodes)[0]
+
+        last_node = self.nodes[len(self.nodes) - 1]
+        for i in range(0, add_nodes):
+            ged = ged + 1
+            self.nodes.append(Node(last_node.id + i + 1, last_node.get_label()))
+
+        nr_edges_added_pop = [0, 1, 2]
+        weight_edges = [0.5, 0.35, 0.15]
+        add_edges = choices(nr_edges_added_pop, weight_edges)[0]
+        for i in range(0, add_edges):
+            ged = ged + 0
+            # self.nodes.append(Node(last_node.id + i + 1, last_node.get_label()))
+        return ged
+
+    def generate_train_file(self):
+        graph2 = deepcopy(self)
+        ged = graph2.edit_graph()
+        graph_dict = {
+            'graph1': [x.as_array() for x in self.edges],
+            'graph2': [x.as_array() for x in graph2.edges],
+            'labels1': [x.get_label() for x in self.nodes],
+            'labels2': [x.get_label() for x in graph2.nodes],
+            'ged': ged
+        }
+        json_object = json.dumps(graph_dict)
+        path = f'dataset/train/{self.filename}'
+        folder_path = path.rsplit('/', 1)[0]
+        path_exist = os.path.exists(folder_path)
+        if not path_exist:
+            os.makedirs(folder_path)
+        with open(path, 'w+') as outfile:
+            outfile.write(json_object)
---
+++ graphNormalizer/main.py
@@ -0,0 +1,60 @@
+import os
+import json
+
+from graph import Node, Graph
+
+
+def read_jsons(path_to_dir):
+    all_json_data = []
+    for file_name in os.listdir(path_to_dir):
+        file_path = f'{path_to_dir}/{file_name}'
+        with open(file_path, 'r') as json_file:
+            json_data = json.load(json_file)
+            json_data['filename'] = file_path
+            all_json_data.append(json_data)
+    return all_json_data
+
+
+def without_keys(d, keys):
+    return {k: v for k, v in d.items() if k not in keys}
+
+
+def get_component_nodes(json_data):
+    component_nodes = []
+    for component in json_data['components']:
+        component_node = Node(component['id'], json.dumps(without_keys(component, ['id', 'name'])))
+        component_nodes.append(component_node)
+    return component_nodes
+
+
+def get_vm_nodes(json_data, starting_index):
+    vm_nodes = []
+    for idx, vm_type in enumerate(json_data['output'][3]['types_of_VMs']):
+        if vm_type == 0:
+            vm_nodes.append(Node(starting_index + idx, '0'))
+        else:
+            vm_price = json_data['output'][4]['prices_of_VMs'][idx]
+            vm_specs = next(
+                (list(x.values())[0] for x in json_data['output'][5]['VMs specs'] if
+                 list(x.values())[0]['price'] == vm_price), 'no spec found'
+            )
+            vm_nodes.append(Node(starting_index + idx, json.dumps(vm_specs)))
+    return vm_nodes
+
+
+def get_graph_data(json_data, file_name):
+    component_nodes = get_component_nodes(json_data)
+    vm_nodes = get_vm_nodes(json_data, len(component_nodes) + 1)
+    adjacency_matrix = json_data['output'][6]['assign_matr']
+    return Graph(file_name, component_nodes, vm_nodes, adjacency_matrix)
+
+
+if __name__ == '__main__':
+    data = read_jsons('secureWebContainerJSONS')
+    graphs = []
+    for json_graph_data in data:
+        filename = json_graph_data['filename']
+        graphs.append(get_graph_data(json_graph_data, filename))
+
+    for graph in graphs:
+        graph.generate_train_file()
